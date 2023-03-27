835199f86fdae85e8ffb18a52df2b193d274732f
norihito.yamazaki <norihito.yamazaki@sansan.com>
2023-03-28 06:06:16 +0900

    first commit

---
+++ KeyPickupApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace KeyPickupApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ KeyPickupApp/Pages/KeyReceivedPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace KeyPickupApp.Pages;
+
+public partial class KeyReceivedPage : ContentPage
+{
+	public KeyReceivedPage()
+	{
+		InitializeComponent();
+	}
+}
