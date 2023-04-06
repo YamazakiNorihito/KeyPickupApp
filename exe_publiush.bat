dc5469b634de760fa0034d455914a35034cc329d
norihito.yamazakis <kizinn1224@gmail.com>
2023-04-06 22:33:15 +0900

    発行バッチ作成

--- exe_publiush.bat
+++ exe_publiush.bat
@@ -1,1 +1,11 @@
-msbuild .\KeyPickupApp.sln /restore /t:build /p:Configuration=Release /p:Platform="Any CPU" /p:TargetFramework=net6.0-windows10.0.19041 /p:WindowsAppSDKSelfContained=true /p:WindowsPackageType=None /p:RuntimeIdentifier=win10-x64
+@echo off
+setlocal
+
+set MSBUILD_EXE=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe
+set SOLUTION_FILE=.\KeyPickupApp.sln
+set TARGET_FRAMEWORK=net6.0-windows10.0.19041
+set RUNTIME_IDENTIFIER=win10-x64
+
+call "%MSBUILD_EXE%" "%SOLUTION_FILE%" /restore /t:build /p:Configuration=Release /p:Platform="Any CPU" /p:TargetFramework="%TARGET_FRAMEWORK%" /p:WindowsAppSDKSelfContained=true /p:WindowsPackageType=None /p:RuntimeIdentifier="%RUNTIME_IDENTIFIER%"
+
+endlocal
